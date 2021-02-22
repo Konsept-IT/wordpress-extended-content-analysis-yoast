567e47080f78d14c560a43116d91bf0696762279
Alexander Sagen <alexander@sagen.me>
2021-02-22 10:03:39 +0100

    Tidy up code, improve AJAX method security

--- admin/js/extend-content-yoast.js
+++ admin/js/extend-content-yoast.js
@@ -1,75 +1,55 @@
-jQuery(document).ready(function($){
-    
-    
+jQuery(document).ready(function($) {
     /*
     *   get URL parameter
     */
-    var getUrlParameter = function getUrlParameter(sParam) {
-        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
             sURLVariables = sPageURL.split('&'),
             sParameterName,
             i;
-    
         for (i = 0; i < sURLVariables.length; i++) {
             sParameterName = sURLVariables[i].split('=');
-    
-            if (sParameterName[0] === sParam) {
-                return sParameterName[1] === undefined ? true : sParameterName[1];
+
+            if (decodeURIComponent(sParameterName[0]) === sParam) {
+                return typeof sParameterName[1] === 'undefined' || sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
             }
         }
+        return null;
     };
-    
-    
-    
-    
-    
+
     /*
     *   global vars
     */
     var postParameter   = getUrlParameter('post'),
         actionParameter = getUrlParameter('action');
 
-
-
-
     /*
     *   YOAST-SEO.JS
     *   https://github.com/Yoast/YoastSEO.js
     */
-    
-    
+
     // vars
-    var extendContentAnalysis = function () {
-        
+    var extendContentAnalysis = function() {
         // register plugin
         YoastSEO.app.registerPlugin('extendContentAnalysis', {status: 'ready'});
-        
+
         // get new content
         this.fetchData();
-        
     };
 
-
     // get post content
-    extendContentAnalysis.prototype.fetchData = function () {
-        
-        var _self = this,
-            _postData   = {postID: postParameter},
-            ajaxurl     = extendContentYoastScript.pluginsUrl + '/admin/get-post-content.php'; // the url where we want to POST
-                        
+    extendContentAnalysis.prototype.fetchData = function() {
+
+        var _self = this;
+
         /* Return AJAX call to fetch your string content */
-        return $.ajax({
+        return $.post({
             url: ajaxurl,
             type: 'post',
             dataType: 'json',
-            data: _postData
-            
+            data: {postID: postParameter}
         })
-        
-        // get response (post content)
-        .done(function (response) {
-            
-            console.log('success');
+        .done(function(response) {
             _self.extra_content = response.content;
             YoastSEO.app.pluginReady('extendContentAnalysis');
 
@@ -82,62 +62,36 @@ jQuery(document).ready(function($){
              *                                    correspond with earlier execution.
              */
             YoastSEO.app.registerModification('content', $.proxy(_self.getContent, _self), 'extendContentAnalysis', 5);
-
         })
-        
-        // using the fail promise callback
         .fail(function(data) {
-            
-            console.log('fail');
-            console.log(data);
-            
+            console.error('extent-content-yoast: Unable to load post content data:', data);
         });
-        
+
     };
-    
-    
-    
-    
+
     /**
     * Adds some text to the content
     *
     * @param content - The data to modify
     */
-    extendContentAnalysis.prototype.getContent = function (content) {
-            // plugin fetched content
+    extendContentAnalysis.prototype.getContent = function(content) {
+        // plugin fetched content
         var currentContent  = content,
             // our new fetched content
             newContent      = this.extra_content;
-        
-        
-        if(newContent){
-            return newContent;
-        } else {
-            return currentContent;
-        }
 
-        //return this.extra_content ? ( this.extra_content + content ) : content;
+        if (newContent){
+            return newContent;
+        }
+        return currentContent;
     };
-    
-    
-    
 
     /**
      * YoastSEO content analysis integration
     */
     if(postParameter && actionParameter == 'edit'){
-        
-        $(window).on('YoastSEO:ready', function () {
+        $(window).on('YoastSEO:ready', function() {
             new extendContentAnalysis();
         });
-        
     }
-
-
-
-    
-}); /* ready END */
-
-
-
-
+});
--- extended-content-analysis-yoastseo.php
+++ extended-content-analysis-yoastseo.php
@@ -1,65 +1,77 @@
 <?php
 /**
  * @link              http://www.ressourcenmangel.de/
- * @since             1.0.0
+ * @since             1.0.1
  * @package           extended-content-analysis-yoastseo
  *
  * @wordpress-plugin
  * Plugin Name:       Extended Content Analysis for YOAST SEO
  * Plugin URI:        https://github.com/Basbee
  * Description:       Plugin extends the "Content Analysis" Features of YOAST SEO while it fetches the complete Post or Page content manually and even gets the data out of layout building tools like Enfolds Avia Layout Builder.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Sebastian Kulahs
  * Author URI:        https://github.com/Basbee
+ * Author:            Alexander Sagen <alexander@konsept-it.no>
+ * Author URI:        https://github.com/Konsept-IT
  * Text Domain:       extended-content-analysis-yoastseo
  */
 
-
-
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if (!defined('WPINC')) {
 	die;
 }
 
-
 // manually require plugin.php in order to use is_plugin_active
-include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+include_once(ABSPATH . 'wp-admin/includes/plugin.php');
 
-
-
-
-/*
- *  Check if YOAST SEO Plugin is installed and activated
+/**
+ * Check if YOAST SEO Plugin is installed and activated
  */
-if ( !is_plugin_active( 'wordpress-seo/wp-seo.php' ) ) {
-
-    
-    /*
-    *   display message if YOAST SEO is not activated
-    */
-    function extend_content_yoast_admin_notices() {
-            
-            echo '<div class="updated"><p>YOAST SEO Plugin ( min v.3.0 ) must be activated in order to get <strong>"Extended Content Analysis for YOAST SEO"</strong> work correctly.</p></div>';        
-    
-    }
-    add_action('admin_notices', 'extend_content_yoast_admin_notices');    
-
-    
-} else{
-    
-    
-    /*
-    *   Register scripts - add child theme javascripts to admin area only
-    */
-    function extend_content_yoast_scripts() {
-        
-        wp_register_script( 'extend-content-analysis', plugins_url( '/admin/js/extend-content-yoast.js', __FILE__ ) );
-    	wp_enqueue_script( 'extend-content-analysis', plugins_url( '/admin/js/extend-content-yoast.js', __FILE__ ), array('yoast-seo'), '1.0.0', true );
-    	// register var pluginsUrl for usage in JS files
-    	wp_localize_script('extend-content-analysis', 'extendContentYoastScript', array('pluginsUrl' => plugins_url('', __FILE__),));
-    	
-    }
-    add_action('admin_enqueue_scripts', 'extend_content_yoast_scripts');
-    
-    
+if (!is_plugin_active('wordpress-seo/wp-seo.php')) {
+	/*
+	 *   display message if YOAST SEO is not activated
+	 */
+	function extend_content_yoast_admin_notices() {
+		echo '<div class="updated"><p>YOAST SEO Plugin ( min v.3.0 ) must be activated in order to get <strong>"Extended Content Analysis for YOAST SEO"</strong> work correctly.</p></div>';
+	}
+	add_action('admin_notices', 'extend_content_yoast_admin_notices');
+	return;
 }
+
+/**
+ * Register scripts - add child theme javascripts to admin area only
+ */
+function extend_content_yoast_scripts() {
+	wp_register_script('extend-content-analysis', plugins_url( '/admin/js/extend-content-yoast.js', __FILE__ ));
+	wp_enqueue_script('extend-content-analysis', plugins_url( '/admin/js/extend-content-yoast.js', __FILE__ ), array('yoast-seo'), '1.0.1', true);
+}
+add_action('admin_enqueue_scripts', 'extend_content_yoast_scripts');
+
+/**
+ * Add AJAX method
+ */
+function extend_content_yoast_ajax_get_post_content() {
+	$post_id = intval($_POST['postID']);
+
+	// Check user access to edit post (this plugin is only used when editing posts)
+	if (!current_user_can('edit_post', $post_id)) wp_die('0', 403);
+
+	// Get post by ID
+	$post = get_page($post_id);
+	if (empty($post) || !is_object($post)) wp_die('0', 404);
+
+	// Get post HTML
+	$content = apply_filters('the_content', $post->post_content);
+
+	// Replace characters to get correct number of words count
+	// - Remove HTML entities from content string
+	$content = preg_replace("/&#?[a-z0-9]{2,8};/i", "", $content);
+	// - Remove whitespaces after html tags from content string
+	$content = preg_replace("/>\s+/", '>', $content);
+
+	// return post content in json format
+	header("Content-type: application/json; charset=utf-8");
+	echo json_encode(array('content' =>  $content), JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+	wp_die();
+}
+add_action('wp_ajax_extend-content-yoast-get-post-content', 'extend_content_yoast_ajax_get_post_content');
